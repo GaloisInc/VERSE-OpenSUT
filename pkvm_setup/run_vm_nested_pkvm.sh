48e41e6da3ef098c983633a7127124856a776be8
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-01 13:40:25 -0700

    pkvm_setup: add missing pkvm kernel arguments

--- pkvm_setup/run_vm_nested_pkvm.sh
+++ pkvm_setup/run_vm_nested_pkvm.sh
@@ -10,4 +10,4 @@ exec bash "$(dirname "$0")/run_vm_common.sh" \
   -drive if=virtio,format=qcow2,file="$disk_guest" \
   -kernel vms/pkvm-boot/vmlinuz-pkvm \
   -initrd vms/debian-boot/initrd.img \
-  -append 'earlycon root=/dev/vda2'
+  -append 'earlycon root=/dev/vda2 nokaslr kvm-arm.mode=protected'
