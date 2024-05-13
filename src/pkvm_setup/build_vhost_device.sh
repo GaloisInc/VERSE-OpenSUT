3b156b50d8c05995c4e9c9dd9164f82b4748c24e
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-13 16:47:07 -0700

    pkvm_setup: add vhost-device submodule, deps, and build script

---
+++ src/pkvm_setup/build_vhost_device.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+# Dependencies:
+# - A recent Rust stable toolchain (tested with 1.78.0)
+# - libgpiod (run `build_libgpiod.sh` first)
+
+export SYSTEM_DEPS_LIBGPIOD_NO_PKG_CONFIG=1
+export SYSTEM_DEPS_LIBGPIOD_SEARCH_NATIVE="$(pwd)/libgpiod/lib/.libs/"
+export SYSTEM_DEPS_LIBGPIOD_LIB=gpiod
+export SYSTEM_DEPS_LIBGPIOD_INCLUDE="$(pwd)/libgpiod/include/"
+
+cd vhost-device
+
+# If rustup is installed, this will cause it to use the stable toolchain (if
+# the caller hasn't already set `RUSTUP_TOOLCHAIN` to somtehing else).  Without
+# rustup, this does nothing.
+: ${RUSTUP_TOOLCHAIN=stable}
+export RUSTUP_TOOLCHAIN
+
+cargo build --bin vhost-device-gpio
+#cargo build --bin vhost-device-i2c
