dc18ccc2358504544b825a8f657fcd13caecaa11
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-14 10:47:39 -0700

    pkvm_setup: fix shellcheck lints

--- src/pkvm_setup/build_vhost_device.sh
+++ src/pkvm_setup/build_vhost_device.sh
@@ -10,17 +10,18 @@ if ! [[ -f libgpiod/lib/.libs/libgpiod.so ]] && ! [[ -f libgpiod/lib/.libs/libgp
     exit 1
 fi
 
+pwd="$(pwd)"
 export SYSTEM_DEPS_LIBGPIOD_NO_PKG_CONFIG=1
-export SYSTEM_DEPS_LIBGPIOD_SEARCH_NATIVE="$(pwd)/libgpiod/lib/.libs/"
+export SYSTEM_DEPS_LIBGPIOD_SEARCH_NATIVE="$pwd/libgpiod/lib/.libs/"
 export SYSTEM_DEPS_LIBGPIOD_LIB=gpiod
-export SYSTEM_DEPS_LIBGPIOD_INCLUDE="$(pwd)/libgpiod/include/"
+export SYSTEM_DEPS_LIBGPIOD_INCLUDE="$pwd/libgpiod/include/"
 
 cd vhost-device
 
 # If rustup is installed, this will cause it to use the stable toolchain (if
 # the caller hasn't already set `RUSTUP_TOOLCHAIN` to somtehing else).  Without
 # rustup, this does nothing.
-: ${RUSTUP_TOOLCHAIN=stable}
+: "${RUSTUP_TOOLCHAIN=stable}"
 export RUSTUP_TOOLCHAIN
 
 cargo build --bin vhost-device-gpio
