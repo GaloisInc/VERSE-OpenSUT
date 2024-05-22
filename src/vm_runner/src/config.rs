ca87c272b767595e9cd9541bfb5ee889eecdc8d4
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-22 14:36:22 -0700

    initial commit of vm_runner

---
+++ src/vm_runner/src/config.rs
@@ -0,0 +1,44 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Config {
+    pub mode: Mode,
+    #[serde(default)]
+    pub processes: Vec<Process>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Mode {
+    /// Manage multiple processes running concurrently.  Any number of processes is permitted.  All
+    /// processes will be started, and the runner will wait for all of them to exit.  If a process
+    /// exits unsuccessfully, all other processes will be terminated.
+    Manage,
+    /// `exec` a single command.  There must be exactly one process in the config file.
+    Exec,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Process {
+    Shell(ShellProcess),
+    Vm(VmProcess),
+}
+
+/// Run a shell command.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct ShellProcess {
+    pub command: String,
+}
+
+/// Spawn a VM.
+///
+/// This could instead be done using a `ShellProcess` that invokes QEMU, but using `type = "vm"`
+/// handles the most common device options automatically.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct VmProcess {
+    // TODO
+}
