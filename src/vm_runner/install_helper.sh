6036073dad140ff5cf6af721f82a6de3dd413602
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-23 15:42:42 -0700

    vm_runner: add scripts for building and installing inside a VM

---
+++ src/vm_runner/install_helper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+# Script for building the `vm_runner` project inside a VM.
+
+if [[ "$(id -u)" -eq "0" ]]; then
+    # Drop privileges for the rest of the script.
+    cp "$0" /tmp/vm-script
+    chown user:user /tmp/vm-script
+    exec sudo -u user /bin/bash /tmp/vm-script "$@"
+fi
+
+cd ~
+
+mkdir -p vm_runner
+sudo mount -t 9p -o trans=virtio,version=9p2000.L,rw vm_runner vm_runner
+
+sudo mkdir -p /opt/opensut/bin
+target=aarch64-unknown-linux-gnu
+sudo cp -v \
+    vm_runner/target/"$target"/release/opensut_vm_runner \
+    vm_runner/target/"$target"/release/opensut_boot \
+    /opt/opensut/bin
+
+# TODO: install systemd unit and configure to run on startup
