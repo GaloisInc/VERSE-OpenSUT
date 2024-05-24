5529c415cfbdec461ec135a839c99ac6f70889d5
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-24 13:22:25 -0700

    vm_runner: nested version of hello world test

--- src/vm_runner/tests/build_hello_img.sh
+++ src/vm_runner/tests/build_hello_img.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 tests_dir="$(dirname "$0")"
 
 python3 "$tests_dir/../build_application_image.py" \
+    -f "$tests_dir/hello_guest.toml=runner.toml" \
     -f "$tests_dir/hello.sh" \
-    -f "$tests_dir/hello.toml=runner.toml" \
-    -o "$tests_dir/hello.img"
+    -o "$tests_dir/hello_guest.img"
+
+python3 "$tests_dir/../build_application_image.py" \
+    -f "$tests_dir/hello_host.toml=runner.toml" \
+    -f "$tests_dir/hello_guest.img" \
+    -o "$tests_dir/hello_host.img"
+
