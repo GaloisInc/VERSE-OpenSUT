6686ce7abe5d4ab3e29a9dbeb98a712e305095d0
Stuart Pernsteiner <spernsteiner@galois.com>
2024-05-31 10:31:35 -0700

    vm_runner: add configs for running the MPS test suite

---
+++ src/vm_runner/tests/mps_tests/build_img.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+mps_dir="$(dirname "$0")"
+
+python3 "$mps_dir/../../build_application_image.py" \
+    -f "$mps_dir/guest.toml=runner.toml" \
+    -f "$mps_dir/../../../../components/mission_protection_system/src/rts.self_test.aarch64=src/rts.self_test" \
+    -f "$mps_dir/../../../../components/mission_protection_system/src/rts.no_self_test.aarch64=src/rts.no_self_test" \
+    -d "$mps_dir/../../../../components/mission_protection_system/tests" \
+    -f "$mps_dir/mps_tests.sh" \
+    -o "$mps_dir/guest.img"
+
+python3 "$mps_dir/../../build_application_image.py" \
+    -f "$mps_dir/host.toml=runner.toml" \
+    -f "$mps_dir/guest.img" \
+    -o "$mps_dir/host.img"
+
