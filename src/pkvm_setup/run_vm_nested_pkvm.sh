3ef873ab9634cef3346477e6157fe50d05cef025
Stuart Pernsteiner <spernsteiner@galois.com>
2024-06-24 11:16:59 -0700

    pkvm_setup: update docs and example scripts

--- src/pkvm_setup/run_vm_nested_pkvm.sh
+++ src/pkvm_setup/run_vm_nested_pkvm.sh
@@ -8,6 +8,6 @@ shift 2
 exec bash "$(dirname "$0")/run_vm_common.sh" \
   -drive if=virtio,format=qcow2,file="$disk_host" \
   -drive if=virtio,format=qcow2,file="$disk_guest" \
-  -kernel vms/pkvm-boot/vmlinuz-pkvm \
+  -kernel vms/pkvm-boot/vmlinuz \
   -initrd vms/debian-boot/initrd.img \
   -append 'earlycon root=/dev/vda2 nokaslr kvm-arm.mode=protected'
