e1461c8be5938061c77c94e31607b270485e3fe0
Stuart Pernsteiner <spernsteiner@galois.com>
2024-07-12 09:44:47 -0700

    trusted_boot: add makefile

---
+++ components/include/wars.h
@@ -0,0 +1,22 @@
+#ifndef __CN_WARS_H__
+#define __CN_WARS_H__
+// general things not yet supported by CN
+
+// define that we are using CN for code checking
+// NOTE: eventually we don't want to differentiate between the checked
+// code and the deployed code
+#define CN_ENV 1
+
+// CN issue https://github.com/rems-project/cerberus/issues/284
+// GCC atributes not supported
+#define WAR_CN_284 1
+
+// CN issue https://github.com/rems-project/cerberus/issues/285
+// Function pointers not supported
+#define WAR_CN_285 1
+
+// CN issue https://github.com/GaloisInc/VERSE-Toolchain/issues/103
+// memcmp not supported
+#define WAR_VERSE_TOOLCHAIN_103 1
+
+#endif // __CN_WARS_H__
