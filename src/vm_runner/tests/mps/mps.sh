320fc4c15159c8d510a6a4f0ba1d824a71ce7578
Stuart Pernsteiner <spernsteiner@galois.com>
2024-07-16 11:06:18 -0700

    mps: enable gpio for vm tests

--- src/vm_runner/tests/mps/mps.sh
+++ src/vm_runner/tests/mps/mps.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 mps_bin="$(dirname "$0")/mps"
 sha256sum "$mps_bin"
 echo "Starting mps"
-setsid -c "$mps_bin" </dev/hvc0 >/dev/hvc0 2>/dev/hvc0
+setsid -c "$mps_bin" </dev/hvc0 >/dev/hvc0
 echo "mps exited with code $?"
