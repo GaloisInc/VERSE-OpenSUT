20a9997f64dd14f217f21c0a2a62470c8b96aa0a
Stuart Pernsteiner <spernsteiner@galois.com>
2024-08-09 16:33:23 -0700

    pkvm_setup: factor out common logic in create_disk_images.sh

--- src/pkvm_setup/create_disk_images.sh
+++ src/pkvm_setup/create_disk_images.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 set -euo pipefail
 
+# This script builds the disk images for all of the OpenSUT host and guest VMs.
+# These contain some common dependencies and configuration, but no application
+# code; applications such as MPS are provided as a separate "applicastion
+# partition" when running the VM.  (The difference is that the VM image created
+# by this script is considered "trusted", but the application image is not and
+# must be integrity-checked before use.)
+#
+# To keep the total size of the images under control, we use QEMU's transparent
+# compression and snapshot/delta features.  We build a tree of images, where
+# each disk image in the tree (except the root) is a delta based on its parent
+# image.  Each image is also compressed once we're done writing to it (writing
+# to a compressed image may overwrite compressed blocks with uncompressed
+# ones).
+#
+# Here is the tree of images we create:
+#
+# * common
+#   * host
+#   * host_dev
+#   * guest
+#   * guest_dev
+#
+# `host_dev` and `guest_dev` are generic host/guest images for development and
+# testing.  The remaining `host*` and `guest*` images are used for running the
+# OpenSUT.  The `common*` images contain packages common to multiple host/guest
+# images and are used to keep the total size of the images down.
+
 mkdir -p vms
 
-disk_base=vms/disk_base.img
-disk_common=vms/disk_common.img
-disk_host=vms/disk_host.img
-disk_guest=vms/disk_guest.img
-disk_host_dev=vms/disk_host_dev.img
-disk_guest_dev=vms/disk_guest_dev.img
+disk() {
+    echo "vms/disk_$1.img"
+}
 
 
 pkvm_version=6.4.0
@@ -89,85 +113,113 @@ find_linux_image_deb() {
 }
 
 
+define_image() {
+    local name="$1"
+    local disk
+    disk="$(disk "$name")"
+
+    if [[ -e "$disk" ]]; then
+        echo "keeping existing $disk" 1>&2
+    else
+        if ! [[ -e "$disk.orig" ]]; then
+            "do_img_$name"
+        fi
+        compress_helper "$disk" "$name"
+    fi
+}
+
+define_image_readonly() {
+    local name="$1"
+    local disk
+    disk="$(disk "$name")"
+
+    if [[ -e "$disk" ]]; then
+        echo "keeping existing $disk" 1>&2
+    else
+        if ! [[ -e "$disk.orig" ]]; then
+            "do_img_$name"
+        fi
+        compress_helper "$disk" "$name"
+        edo chmod -v a-w "$disk"
+    fi
+}
+
+define_image_copy() {
+    local src="$1"
+    local dest="$2"
+
+    if [[ -e "$(disk "$dest")" ]]; then
+        echo "keeping existing $(disk "$dest")" 1>&2
+    else
+        edo cp -v "$(disk "$src")" "$(disk "$dest")"
+    fi
+}
+
+
 # `disk_common` is a copy of `disk_base` with additional software and
 # configuration that's common to both the host and the guest.  It's also
 # cleaned and trimmed to reduce its compressed size.
-if [[ -e "$disk_common" ]]; then
-    echo "keeping existing $disk_common" 1>&2
-else
-    if ! [[ -e "$disk_common.orig" ]]; then
-        # Copy instead of using `derive_image` so `fstrim` can trim the combine
-        # base+common image.  `fstrim` can only trim the final read-write image
-        # in a backing chain.
-        edo cp -v "$disk_base" "$disk_common.orig"
 
-        # Prepare storage for the custom packages and the extracted kernel and
-        # initrd images.
-        tar_file=$(mktemp "$(pwd)/kernel.XXXXXX.tar")
-        edo dd if=/dev/zero of="$tar_file" bs=1M count=256
+do_img_common() {
+    # Copy instead of using `derive_image` so `fstrim` can trim the combine
+    # base+common image.  `fstrim` can only trim the final read-write image
+    # in a backing chain.
+    edo cp -v "$(disk base)" "$(disk common).orig"
 
-        tar_inputs=(
-            # linux-pkvm kernel
-            "$(find_linux_image_deb ${pkvm_version} pkvm ${pkvm_rev})"
-            # opensut_boot
-            "$(sole_file ../vm_runner/verse-opensut-boot_[0-9]*_arm64.deb)"
-            # vhost-device
-            "$(sole_file vhost-device/verse-vhost-device-gpio_[0-9]*_arm64.deb)"
-            #"$(sole_file vhost-device/verse-vhost-device-i2c_[0-9]*_arm64.deb)"
-            # Could add more packages if needed, e.g. linux-headers
-        )
-        edo tar --transform='s:.*/::g' -c "${tar_inputs[@]}" | edo dd of="$tar_file" conv=notrunc
+    # Prepare storage for the custom packages and the extracted kernel and
+    # initrd images.
+    tar_file=$(mktemp "$(pwd)/kernel.XXXXXX.tar")
+    edo dd if=/dev/zero of="$tar_file" bs=1M count=256
 
-        edo bash run_vm_script.sh "$disk_common.orig" vm_scripts/setup_common.sh "$tar_file"
+    tar_inputs=(
+        # linux-pkvm kernel
+        "$(find_linux_image_deb ${pkvm_version} pkvm ${pkvm_rev})"
+        # opensut_boot
+        "$(sole_file ../vm_runner/verse-opensut-boot_[0-9]*_arm64.deb)"
+        # vhost-device
+        "$(sole_file vhost-device/verse-vhost-device-gpio_[0-9]*_arm64.deb)"
+        #"$(sole_file vhost-device/verse-vhost-device-i2c_[0-9]*_arm64.deb)"
+        # Could add more packages if needed, e.g. linux-headers
+    )
+    edo tar --transform='s:.*/::g' -c "${tar_inputs[@]}" | edo dd of="$tar_file" conv=notrunc
 
-        edo mkdir -p vms/pkvm-boot
-        edo tar -C vms/pkvm-boot -xf "$tar_file"
-        edo rm -f "$tar_file"
-    fi
-    compress_helper "$disk_common" common
-    # Mark `disk_common` read-only.  It's used as a backing file for
-    # `disk_host` and `disk_guest`, so modifying it would cause data
-    # corruption.
-    edo chmod -v a-w "$disk_common"
-fi
+    edo bash run_vm_script.sh "$(disk common).orig" vm_scripts/setup_common.sh "$tar_file"
+
+    edo mkdir -p vms/pkvm-boot
+    edo tar -C vms/pkvm-boot -xf "$tar_file"
+    edo rm -f "$tar_file"
+}
+define_image_readonly common
 
 
 # `disk_host` and `disk_guest` is a delta on top of `disk_common` with host-
 # or guest-specific software.
 
-if [[ -e "$disk_host" ]]; then
-    echo "keeping existing $disk_host" 1>&2
-else
-    if ! [[ -e "$disk_host.orig" ]]; then
-        edo derive_image "$disk_common" "$disk_host.orig"
-        edo bash change_uuids.sh "$disk_common" "$disk_host.orig"
+do_img_host() {
+    edo derive_image "$(disk common)" "$(disk host).orig"
+    edo bash change_uuids.sh "$(disk common)" "$(disk host).orig"
 
-        tar_file=$(mktemp "$(pwd)/host.XXXXXX.tar")
-        tar_inputs=(
-            # qemu-system-arm and dependencies
-            "$(sole_file qemu_build/bookworm-arm64_result/qemu-system-arm_*-9999+verse*_arm64.deb)"
-            "$(sole_file qemu_build/bookworm-arm64_result/qemu-system-common_*-9999+verse*_arm64.deb)"
-            "$(sole_file qemu_build/bookworm-arm64_result/qemu-system-data_*-9999+verse*_all.deb)"
-        )
-        edo tar --transform='s:.*/::g' -cf "$tar_file" "${tar_inputs[@]}"
+    tar_file=$(mktemp "$(pwd)/host.XXXXXX.tar")
+    tar_inputs=(
+        # qemu-system-arm and dependencies
+        "$(sole_file qemu_build/bookworm-arm64_result/qemu-system-arm_*-9999+verse*_arm64.deb)"
+        "$(sole_file qemu_build/bookworm-arm64_result/qemu-system-common_*-9999+verse*_arm64.deb)"
+        "$(sole_file qemu_build/bookworm-arm64_result/qemu-system-data_*-9999+verse*_all.deb)"
+    )
+    edo tar --transform='s:.*/::g' -cf "$tar_file" "${tar_inputs[@]}"
 
-        edo bash run_vm_script.sh "$disk_host.orig" vm_scripts/setup_host.sh "$tar_file"
+    edo bash run_vm_script.sh "$(disk host).orig" vm_scripts/setup_host.sh "$tar_file"
 
-        edo rm -f "$tar_file"
-    fi
-    compress_helper "$disk_host" host
-fi
+    edo rm -f "$tar_file"
+}
+define_image host
 
-if [[ -e "$disk_guest" ]]; then
-    echo "keeping existing $disk_guest" 1>&2
-else
-    if ! [[ -e "$disk_guest.orig" ]]; then
-        edo derive_image "$disk_common" "$disk_guest.orig"
-        edo bash change_uuids.sh "$disk_common" "$disk_guest.orig"
-        edo bash run_vm_script.sh "$disk_guest.orig" vm_scripts/setup_guest.sh
-    fi
-    compress_helper "$disk_guest" guest
-fi
+do_img_guest() {
+    edo derive_image "$(disk common)" "$(disk guest).orig"
+    edo bash change_uuids.sh "$(disk common)" "$(disk guest).orig"
+    edo bash run_vm_script.sh "$(disk guest).orig" vm_scripts/setup_guest.sh
+}
+define_image guest
 
 
 # `disk_host_dev` and `disk_guest_dev` are copies of `disk_host` and
@@ -175,14 +227,5 @@ fi
 # those images might change (e.g. adding new log entries each time the VM is
 # booted).
 
-if [[ -e "$disk_host_dev" ]]; then
-    echo "keeping existing $disk_host_dev" 1>&2
-else
-    edo cp -v "$disk_host" "$disk_host_dev"
-fi
-
-if [[ -e "$disk_guest_dev" ]]; then
-    echo "keeping existing $disk_guest_dev" 1>&2
-else
-    edo cp -v "$disk_guest" "$disk_guest_dev"
-fi
+define_image_copy host host_dev
+define_image_copy guest guest_dev
