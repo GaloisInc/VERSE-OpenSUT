29e092b8e177de8a8433fefd96830a432a2e6c1c
Stuart Pernsteiner <spernsteiner@galois.com>
2024-11-21 09:54:49 -0800

    mkm: move CN-specific declarations into cn_stubs.h

--- components/mission_key_management/client.c
+++ components/mission_key_management/client.c
@@ -9,70 +9,11 @@
 #include "mkm.h"
 
 #ifndef CN_ENV
-#include <sys/epoll.h>
-#include <unistd.h>
-#include <stdio.h>
+# include <sys/epoll.h>
+# include <unistd.h>
+# include <stdio.h>
 #else
-# define EPOLLIN 1
-# define EPOLLOUT 4
-
-
-#ifndef WAR_CN_309
-// not possible to call this due to CN issue #309
-// this spec isn't right but can't develop it at all without #309
-void perror(const char *msg);
-/*$ spec perror(pointer msg);
-    requires take mi = Owned<char>(msg);
-    ensures take mo = Owned<char>(msg);
-      mi == mo;
-$*/
-#else
-#define perror(...) 0
-#endif
-/*$ spec exit(i32 v);
-    requires true;
-    ensures true;
-$*/
-
-#define fprintf(...) 0
-#define snprintf(...) 0
-
-typedef signed long long ssize_t;
-ssize_t _read(int fildes, void *buf, size_t n);
-/*$ spec _read(i32 fildes, pointer buf, u64 n);
-    // accesses errno;
-    requires
-      take bufi = ArrayBlock_u8(buf, n);
-    ensures
-     return >= -1i64 && return <= (i64)n;
-     // return == -1 -> no owned, all block
-     // return >= 0 < n -> 0 <= owned < return, return <= block < n
-     // return == n -> 0 <= owned < n, return <= block < n
-     take bufo = each(u64 i; (return == -1i64) ? false : (0u64 <= i && i <(u64)return)) {Owned<uint8_t>(array_shift<uint8_t>(buf, i))};
-     take bufb = each(u64 i; (return == -1i64) ? (0u64 <= i && i < n) : ((u64)return <= i && i < n)) {Block<uint8_t>(array_shift<uint8_t>(buf, i))};
-$*/
-#define read(f,b,s) _read(f,b,s)
-
-int close(int fildes);
-int _close(int fildes);
-/*$ spec _close(i32 fildes);
-    requires true;
-    ensures true;
-$*/
-#define close(x) _close(x)
-
-ssize_t write(int fildes, const void *buf, size_t nbyte);
-ssize_t _write(int fildes, const void *buf, size_t nbyte);
-/*$ spec _write(i32 fildes, pointer buf, u64 nbyte);
-  requires
-    ((i32)nbyte) >= 0i32;
-    take bufi = each(u64 i; i >= 0u64 && i < nbyte) {Owned<uint8_t>(array_shift<uint8_t>(buf,i))};
-  ensures
-    take bufo = each(u64 i; i >= 0u64 && i < nbyte) {Owned<uint8_t>(array_shift<uint8_t>(buf,i))};
-    bufi == bufo;
-    return >= -1i64 && return < (i64)nbyte;
-$*/
-#define write(f,b,s) _write(f,b,s)
+# include "cn_stubs.h"
 #endif
 
 
