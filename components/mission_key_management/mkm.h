c04a1e71fbcaa301a41d4d4623805c0d6f2ab141
Stuart Pernsteiner <spernsteiner@galois.com>
2024-12-05 13:15:22 -0800

    mkm: use #defines for message sizes

--- components/mission_key_management/mkm.h
+++ components/mission_key_management/mkm.h
@@ -2,6 +2,10 @@
 
 #include <stdint.h>
 
-#define NUM_SECRET_KEYS 2
+#define KEY_ID_SIZE 1
+#define CHALLENGE_SIZE 32
+#define RESPONSE_SIZE 32
 #define SECRET_KEY_SIZE 32
+
+#define NUM_SECRET_KEYS 2
 const uint8_t* get_mission_key(uint8_t key_id);
